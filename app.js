b19cf720b89e096b35ec45b4aed3f7c5e026b0c9
Jay Momnang <jaymomnang@users.noreply.github.com>
2017-07-13 12:47:50 +0100

    service added

--- app.js
+++ app.js
@@ -5,6 +5,26 @@ var express = require('express'),
     bodyParser = require('body-parser'),
     assert = require('assert');
 
+var http = require('http');
+//The url we want is: 'www.random.org/integers/?num=1&min=1&max=10&col=1&base=10&format=plain&rnd=new'
+var options = {
+  host: 'www.random.org',
+  path: '/integers/?num=1&min=1&max=10&col=1&base=10&format=plain&rnd=new'
+};
+callback = function(response) {
+  var str = '';
+  //another chunk of data has been recieved, so append it to `str`
+  response.on('data', function (chunk) {
+    str += chunk;
+  });
+  //the whole response has been recieved, so we just print it out here
+  response.on('end', function () {
+    console.log(str);
+  });
+}
+
+http.request(options, callback).end();
+
 
 app.engine('html', engines.nunjucks);
 app.set('view engine', 'html');
